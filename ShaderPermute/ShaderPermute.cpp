437f3ba7328c36009fffb9b92ef1b0d17bd13a1c
MrTroble <nifroh56@gmail.com>
2021-12-23 13:01:48 +0100

    Add project files.

---
+++ ShaderPermute/ShaderPermute.cpp
@@ -0,0 +1,12 @@
+﻿// ShaderPermute.cpp : Defines the entry point for the application.
+//
+
+#include "ShaderPermute.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
